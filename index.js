1c6fc3ac47e283eb4a2384f6f9fb8e59d02bae99
John Otander <johnotander@gmail.com>
2014-12-10 22:21:43 -0700

    Add more tests and basic functionality

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 var rgb = require('rgb');
+var wcag = require('wcag-contrast');
+var isBlank = require('is-blank');
 
 module.exports.ratio = ratio;
 module.exports.score = score;
 module.exports.isAccessible = isAccessible;
 
 function ratio(colorOne, colorTwo) {
+  colorOne = getRgbTriplet(colorOne);
+  colorTwo = getRgbTriplet(colorTwo);
 
+  return wcag.rgb(colorOne, colorTwo);
 }
 
 function score(colorOne, colorTwo) {
+  var wcagScore = wcag.score(ratio(colorOne, colorTwo));
 
+  if (isBlank(wcagScore)) {
+    return 'F';
+  } else {
+    return wcagScore;
+  }
 }
 
 function isAccessible(colorOne, colorTwo) {
+  return ratio(colorOne, colorTwo) > 4.5;
+}
 
+function getRgbTriplet(color) {
+  if (typeof color != 'string') {
+    throw new TypeError('get-contrast expects colors as strings');
+  }
+
+  return rgb(color).match(/\((.*)\)/)[1].split(',').slice(0, 3);
 }
--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var contrast = require('..');
 describe('ratio', function() {
 
   it('should calculate the correct ratio', function() {
-    assert.equal(contrast.ratio('#fafafa', 'rgba(0,0,0,.75)'), 10);
+    assert.equal(Math.round(contrast.ratio('#fafafa', 'rgba(0,0,0,.75)')), 20);
   });
 });
 
@@ -12,6 +12,10 @@ describe('score', function() {
 
   it('should calculate the correct WCAG score', function() {
     assert.equal(contrast.score('#fafafa', 'rgba(0,0,0,.75)'), 'AAA');
+  });
+
+  it('should calculate the correct WCAG score', function() {
+    assert.equal(contrast.score('#fafafa', '#fff'), 'F');
   });
 });
 
@@ -23,5 +27,12 @@ describe('isAccessible', function() {
 
   it('should determine that a contrast is accesible when the ratio is above 4.5:1', function() {
     assert.ok(contrast.isAccessible('#fafafa', 'rgba(0,0,0,.75)'));
+  });
+});
+
+describe('getRgbTriplet', function() {
+
+  it('should throw an error when not given a string', function() {
+    assert.throws(contrast.ratio);
   });
 });
