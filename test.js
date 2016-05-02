98c27a3c0173ccc7ccbbba7e254b88d439f421fd
John Otander <johnotander@gmail.com>
2016-05-01 22:44:23 -0600

    Use ava

--- test.js
+++ test.js
@@ -1,71 +1,34 @@
-var assert = require('assert');
-var contrast = require('./');
+import test from 'ava'
+import contrast from './'
 
-describe('ratio', function() {
+test('ratio', t => {
+  t.plan(2)
 
-  it('should calculate the correct ratio', function() {
-    assert.equal(Math.round(contrast.ratio('#fafafa', 'rgba(0,0,0,.75)')), 20);
-  });
+  t.deepEqual(Math.round(contrast.ratio('#fafafa', 'rgba(0,0,0,.75)')), 20)
+  t.ok(contrast.ratio('tomato', 'rebeccapurple'))
+})
 
-  it('should be able to handle named CSS colors', function() {
-    assert.ok(contrast.ratio('tomato', 'rebeccapurple'));
-  });
-});
+test('score', t => {
+  t.plan(2)
 
-describe('score', function() {
+  t.deepEqual(contrast.score('#fafafa', 'rgba(0,0,0,.75)'), 'AAA')
+  t.deepEqual(contrast.score('#fafafa', '#fff'), 'F')
+})
 
-  it('should calculate the correct WCAG score', function() {
-    assert.equal(contrast.score('#fafafa', 'rgba(0,0,0,.75)'), 'AAA');
-  });
+test('isAccessible', t => {
+  t.plan(2)
 
-  it('should calculate the correct WCAG score', function() {
-    assert.equal(contrast.score('#fafafa', '#fff'), 'F');
-  });
-});
+  t.false(contrast.isAccessible('#fafafa', '#fff'))
+  t.true(contrast.isAccessible('#fafafa', 'rgba(0,0,0,.75)'))
+})
 
-describe('isAccessible', function() {
+test('isNotTransparent', t => {
+  t.plan(5)
 
-  it('should determine that a contrast is inaccessible when the ratio is below 4.5:1', function() {
-    assert.equal(contrast.isAccessible('#fafafa', '#fff'), false);
-  });
+  t.throws(() => (contrast.isNotTransparent('rgba(255,255,255,0)')))
+  t.throws(() => (contrast.isNotTransparent('hsl(100,100,100,0)')))
+  t.throws(() => (contrast.isNotTransparent('transparent')))
+  t.throws(() => (contrast.isNotTransparent('#FFFFFF00')))
 
-  it('should determine that a contrast is accesible when the ratio is above 4.5:1', function() {
-    assert.ok(contrast.isAccessible('#fafafa', 'rgba(0,0,0,.75)'));
-  });
-});
-
-describe('getRgbTriplet', function() {
-
-  it('should throw an error when not given a string', function() {
-    assert.throws(contrast.ratio);
-  });
-});
-
-describe('isNotTransparent', function() {
-  it('should throw an error when one or more color is transparent', function() {
-    // Several test assertions for different styles of 'transparent'.
-    assert.throws(function() {
-      // Transparent white rgba.
-      contrast.isNotTransparent('rgba(255,255,255,0)');
-    },
-    /get-contrast cannot contrast transparent colors/);
-    // Transparent hsl.
-    assert.throws(function() {
-      contrast.isNotTransparent('hsl(100,100,100,0)');
-    },
-    /get-contrast cannot contrast transparent colors/);
-    // Transparent named color.
-    assert.throws(function() {
-      contrast.isNotTransparent('transparent');
-    },
-    /get-contrast cannot contrast transparent colors/);
-    // Transparent hex.
-    assert.throws(function() {
-      contrast.isNotTransparent('#FFFFFF00');
-    },
-    /get-contrast cannot contrast transparent colors/);
-
-    // Does not throw when told to ignore
-    assert.ok(contrast.isNotTransparent('rgba(0, 0, 0, 0)', { ignoreAlpha: true }));
-  });
-});
+  t.ok(contrast.isNotTransparent('rgba(0, 0, 0, 0)', { ignoreAlpha: true }))
+})
